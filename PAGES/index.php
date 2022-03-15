bea63f476e9bd7bf22d9d3317540946c5d2463c3
Mert Gunduz <merttgg@gmail.com>
2022-03-15 13:56:41 +0300

    Source & Directory System Added

---
+++ PAGES/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content="With MedCRAN, your hospital and customers are safe, you are always comfortable. Discover Packages That Are Right For You, Plus 50% Discount For The First 5 Months!">
+            <meta name="keywords" content="Hospital Automation, Hospital Systems, Patient Security, Patient Happiness, Bookkeeping, Accounting Software">
+            <meta name="author" content="Mehmet Mert Gunduz">
+            <link rel="stylesheet" href="../STYLES/stylesheet.css">
+            <link rel="shortcut icon" href="../IMAGES/MedCRAN_Logo.png" type="image/x-icon">
+            <title>MedCRAN</title>
+      </head>
+      <body>    
+            <p>Hello World!</p>
+      </body>
+</html>
