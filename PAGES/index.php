05015acb2f55ec862fa8b4b89affebcb1582580c
Mert Gunduz <merttgg@gmail.com>
2022-03-15 15:24:26 +0300

    Header Finished

--- PAGES/index.php
+++ PAGES/index.php
@@ -8,10 +8,22 @@
             <meta name="keywords" content="Hospital Automation, Hospital Systems, Patient Security, Patient Happiness, Bookkeeping, Accounting Software">
             <meta name="author" content="Mehmet Mert Gunduz">
             <link rel="stylesheet" href="../STYLES/stylesheet.css">
-            <link rel="shortcut icon" href="../IMAGES/MedCRAN_Logo.png" type="image/x-icon">
+            <link rel="shortcut icon" href="../IMAGES/MedCRAN_Logo.svg" type="image/x-icon">
             <title>MedCRAN</title>
       </head>
       <body>    
-            <p>Hello World!</p>
+            <header class="index-header">
+                  <div class="logo-div">
+                        <img width="100" src="../IMAGES/MedCRAN_LogoWhite.svg" alt="">
+                        <h1>MedCRAN</h1>
+                  </div>
+                  <div class="header-inputs">
+                        <div class="search-input-div">
+                              <input id="search-textbox" placeholder="Search Users..." type="text" name="donate-textbox">
+                              <a id="search-button" href="#"><img src="../IMAGES/MedCRAN_SearchLogo.svg" alt="Search Icon"></a>
+                        </div>
+                        <input id="database-button" type="button" name="database-button" value="Database Menu">
+                  </div>
+            </header>
       </body>
 </html>
