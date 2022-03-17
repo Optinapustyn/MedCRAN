67d09d8445f08f715f864876b452af53bca8e8fa
Mert Gunduz <merttgg@gmail.com>
2022-03-17 09:56:48 +0300

    Naming Inputs

--- PAGES/login.php
+++ PAGES/login.php
@@ -32,8 +32,9 @@
             <main class="login-main">
                   <h1 class="h1-purple-text font-weight-300">LOGIN</h1>
                   <form id="login-form" action="../SERVER/login_form_script.php" method="POST">
-                        <input type="text" placeholder="Identity Number">
-                        <input type="text" placeholder="Password">
+                        <input type="text" placeholder="Name & Surname" name="name-surname">
+                        <input type="text" placeholder="Identity Number" name="identity-number">
+                        <input type="text" placeholder="Password" name="password">
                         <input type="button" value="Log In">
                         <a href="register.php"><input type="button" value="Create An Account"></a>
                   </form>
